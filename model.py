80082e0607cb31689a7708290e68eaa3eefd9fa3
DeepakR-Sastry <mamillap@ualberta.ca>
2023-06-06 19:40:08 -0600

    added data

--- model.py
+++ model.py
@@ -5,18 +5,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 import seaborn as sns
 
-df = pd.read_csv("data.csv")
-X = df.drop('power_draw', axis=1)
-y = df[["power_draw"]]
+df = pd.read_csv("dataA100.csv")
+X = df.drop(['power_draw', 'env'], axis=1)
+y = df["power_draw"]
+env = df["env"]
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.15)
-# X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,test_size=0.15)
+mse_scores = []
 
+unique_envs = env.unique()
 
+for test_env in unique_envs:
+    # Filter data for training on all envs except the test_env
+    X_train = X[env != test_env]
+    y_train = y[env != test_env]
 
-regressor = RandomForestRegressor(n_estimators = 100, criterion="squared_error")
-regressor.fit(X_train, y_train.values.ravel())
-print()
-y_pred = regressor.predict(X_test)
-mse = mean_squared_error(y_pred, y_test)
-print(mse)
+    # Filter data for testing on the test_env
+    X_test = X[env == test_env]
+    y_test = y[env == test_env]
+
+    regressor = RandomForestRegressor(n_estimators=100, criterion="mse")
+    regressor.fit(X_train, y_train)
+
+    y_pred = regressor.predict(X_test)
+    mse = mean_squared_error(y_pred, y_test)
+    mse_scores.append(mse)
+
+average_mse = np.mean(mse_scores)
+print("Average Mean Squared Error:", average_mse)
